a82e2c5538278792793822a890197596864f807b
GayangaN <2022sp031@univ.jfn.ac.lk>
2026-07-03 13:09:49 +0530

    created new file ✏️

---
+++ src/components/productCard.jsx
@@ -0,0 +1,11 @@
+export default function ProductCard() {
+
+    return (
+        <div>
+            <h1>Apple iPhone</h1>
+            <img src="https://picsum.photos/id/3/200/300"></img>
+            <p>Price - Rs.150, 000/-</p>
+            <button>Buy Now</button>
+        </div>
+    )
+}
