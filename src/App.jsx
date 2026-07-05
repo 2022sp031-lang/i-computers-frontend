8b3b787c2b1f20ebd59a56641279f09dcf8be126
GayangaN <2022sp031@univ.jfn.ac.lk>
2026-07-05 11:40:10 +0530

    modified some files ✏️

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,19 @@ function App() {
 
   return (
     <>
-      <TrendingProducts/>
+      <div className='w-[900px] h-[600px] border-[2px]'>
+
+        <div className='w-[700px] h-[500px] bg-blue-400 flex flex-col items-center justify-center relative'>
+
+          <div className='w-[50px] h-[50px] bg-red-600'></div>
+          <div className='w-[50px] h-[50px] bg-yellow-600 fixed top-[4px]'></div>
+          <div className='w-[50px] h-[50px] bg-green-600 absolute bottom-[5px]'></div>
+          <div className='w-[50px] h-[50px] bg-purple-500'></div>
+          <div className='w-[50px] h-[50px] bg-white'></div>
+
+        </div>
+
+      </div>
     </>
   )
 }
