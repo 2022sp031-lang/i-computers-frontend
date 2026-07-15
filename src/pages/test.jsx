3562a12c5c935d12f7c370fb6e3b9c1eb993d616
2022sp031-lang <2022sp031@univ.jfn.ac.lk>
2026-07-15 19:43:13 +0530

    Modified some files ✏️

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,5 +1,32 @@
+import { useState } from "react";
+
 export default function Test() {
-    return(
-        <div></div>
+    const [emotion, setEmotion] = useState("😀");
+    
+    return (
+        
+        <div className="w-full h-screen flex flex-col justify-center items-center">
+            <div className="w-[200px] h-[200px] bg-white border-[5px] border-black justify-center items-center flex text-6xl">{emotion}</div>
+            <div className="flex flex-row">
+                <button 
+                onClick={
+                    ()=> setEmotion("😀")}
+                className="bg-accent border-white border-[1px] flex-col text-black">Happy</button>
+
+                <button 
+
+                onClick={
+                    ()=> setEmotion("😒")
+                }    
+
+                className="bg-accent border-white border-[1px] flex-col text-black">Sad</button>
+
+                <button
+                onClick={
+                    ()=> setEmotion("😍")
+                }
+                className="bg-accent border-white border-[1px] flex-col text-black">Love</button>
+            </div>
+        </div>
     )
 }
